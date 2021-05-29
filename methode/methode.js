a91a588bbc87c88cc0dc654d5065418d7a828019
ncochon <ncochon@hotmail.com>
2021-05-29 17:32:33 +0200

    Gestion Methode

--- methode/methode.js
+++ methode/methode.js
@@ -1,8 +1,11 @@
+const NB_ANCIEN_COURS = 2;
+
 function onSignIn() {
     const urlParams = new URLSearchParams(window.location.search);
     var idClient = urlParams.get('idClient');
     if (!idClient) {
-        idClient = 'recGMIxbX0mMzLVGS';
+        //idClient = 'recGMIxbX0mMzLVGS';
+        idClient = 'rec0BYexFc3OIKXUO';
     }
 
     var Airtable = require('airtable');
@@ -24,8 +27,13 @@ function onSignIn() {
                 const name = record.get('Name');
                 $("#clientName").text(name);
 
-                //Charge les 2 derniers cours du client
-                base('Cours').select({ filterByFormula: 'IS_BEFORE({Date}, NOW())', fields: ["Date", ...methodes.map(x => x.titre)], maxRecords: 2, sort: [{ field: "Date", direction: "desc" }] })
+                //Charge les x derniers cours du client
+                base('Cours').select({
+                    filterByFormula: 'AND({Client}="' + name + '", IS_BEFORE({Date}, NOW()))',
+                    fields: ["Date", ...methodes.map(x => x.titre)],
+                    maxRecords: NB_ANCIEN_COURS,
+                    sort: [{ field: "Date", direction: "desc" }]
+                })
                     .firstPage(function (err, records) {
                         if (err) { console.error(err); return; }
                         records.forEach(function (record) {
@@ -35,35 +43,77 @@ function onSignIn() {
                             });
                             cours.unshift(c);
                         });
+                        //Complete si besoin
+                        while (cours.length < NB_ANCIEN_COURS) cours.unshift({});
 
                         //Charge le prochain cours
-                        base('Cours').select({ filterByFormula: 'IS_AFTER({Date}, NOW())', fields: ["Date", ...methodes.map(x => x.titre)], maxRecords: 1, sort: [{ field: "Date", direction: "asc" }] })
+                        base('Cours').select({
+                            filterByFormula: 'AND({Client}="' + name + '", IS_AFTER({Date}, NOW()))',
+                            fields: ["Date", ...methodes.map(x => x.titre)],
+                            maxRecords: 1,
+                            sort: [{ field: "Date", direction: "asc" }]
+                        })
                             .firstPage(function (err, records) {
                                 if (err) { console.error(err); return; }
                                 records.forEach(function (record) {
-                                    let c = { date: record.get('Date') };
+                                    let c = {
+                                        id: record.id,
+                                        date: record.get('Date')
+                                    };
                                     methodes.forEach(x => {
                                         c[x.titre] = record.get(x.titre);
                                     });
                                     cours.push(c);
                                 });
+                                //Complete si besoin
+                                if (cours.length < NB_ANCIEN_COURS + 1) cours.push({});
+
+                                //Colonne avec les dates des cours
+                                for (let i = 0; i < cours.length; i++) {
+                                    $("tr").append("<th>" + (cours[i].date && moment(cours[0].date).format("DD/MM/YYYY")) + "</th>");
+                                }
 
                                 //Créé les dataTables
-                                $("[cours='2']").text(moment(cours[0].date).format("DD/MM/YYYY"));
-                                $("[cours='1']").text(moment(cours[1].date).format("DD/MM/YYYY"));
-                                $("[cours='0']").text(moment(cours[2].date).format("DD/MM/YYYY"));
-                                methodes.forEach(x => {
-                                    var t = $("#tableMethode").clone().appendTo("#divCol").prop("id", x.titre).removeClass("d-none");
-                                    t.DataTable({
-                                        dom: "<'row'<'col-12'tr>>",
-                                        paging: false,
-                                        data: x.pieces.map(p => ({
+                                methodes.forEach(methode => {
+                                    let div1 = $("#container > div:nth-child(1)").clone().appendTo("#container").show();
+                                    let div2 = div1.children("div");
+                                    let btn = div1.children("button").text(methode.titre);
+                                    let t = div2.children("table").prop("id", methode.titre);
+                                    let data = methode.pieces.map(p => {
+                                        let r = new RegExp('^' + p.titre + '$', 'm');
+                                        let result = {
                                             lecon: p.lecon,
-                                            titre: p.titre,
-                                            cours2: false,
-                                            cours1: false,
-                                            cours0: false,
-                                        })),
+                                            titre: p.titre
+                                        };
+                                        //Teste si la leçon est présente
+                                        for (let i = 0; i < cours.length; i++) {
+                                            result['cours' + i] = r.test(cours[i][methode.titre])
+                                        }
+                                        return result;
+                                    });
+
+                                    //Definition des colonnes des anciens cours
+                                    let colonnesCours = [];
+                                    for (let i = 0; i < NB_ANCIEN_COURS; i++) {
+                                        colonnesCours.push({
+                                            data: "cours" + i,
+                                            orderable: false,
+                                            width: "100px",
+                                            className: "dt-center",
+                                            render: function (data, type, row, meta) {
+                                                let checked = "";
+                                                if (data) checked = " checked"
+                                                return '<input type="checkbox" disabled' + checked + '/>';
+                                            },
+                                        })
+                                    }
+
+                                    let dt = t.DataTable({
+                                        dom: "<'row'<'col-12'tr>>",
+                                        paging: true,
+                                        scrollY: "400px",
+                                        scroller: true,
+                                        data: data,
                                         columns: [
                                             {
                                                 data: "lecon",
@@ -73,37 +123,64 @@ function onSignIn() {
                                                 data: "titre",
                                                 orderable: false,
                                             },
+                                            ...colonnesCours,
                                             {
-                                                data: "cours2",
+                                                data: "cours" + NB_ANCIEN_COURS,
                                                 orderable: false,
+                                                width: "100px",
+                                                className: "dt-center",
                                                 render: function (data, type, row, meta) {
                                                     let checked = "";
                                                     if (data) checked = " checked"
-                                                    return '<input type="checkbox" disabled' + checked + '/>';
-                                                },
-                                            },
-                                            {
-                                                data: "cours1",
-                                                orderable: false,
-                                                render: function (data, type, row, meta) {
-                                                    let checked = "";
-                                                    if (data) checked = " checked"
-                                                    return '<input type="checkbox" disabled' + checked + '/>';
-                                                },
-                                            },
-                                            {
-                                                data: "cours0",
-                                                orderable: false,
-                                                render: function (data, type, row, meta) {
-                                                    return '<input type="checkbox" />';
+                                                    return '<input type="checkbox"' + checked + '/>';
                                                 },
                                                 createdCell: function (cell, cellData, rowData, rowIndex, colIndex) {
-                                                    $(cell).find("input").click(() => {
-
+                                                    $(cell).find("input").click(e => {
+                                                        let c = cours[NB_ANCIEN_COURS];
+                                                        let tab = (c[methode.titre] && c[methode.titre].split(String.fromCharCode(10)).filter(onlyUnique)) || [];
+                                                        if (e.target.checked) {
+                                                            //Ajoute la leçon
+                                                            tab.push(rowData.titre);
+                                                        }
+                                                        else {
+                                                            //Retire la leçon
+                                                            tab = tab.filter(titre => titre != rowData.titre);
+                                                        }
+                                                        c[methode.titre] = tab.join(String.fromCharCode(10));
+                                                        //Enregistre
+                                                        base('Cours').update([{
+                                                            id: c.id,
+                                                            fields: {
+                                                                [methode.titre]: c[methode.titre],
+                                                            }
+                                                        }]);
                                                     });
                                                 }
                                             },
                                         ]
+                                    });
+
+                                    let indexLecon = data.findIndex(d => {
+                                        for (let i = 1; i < cours.length; i++) {
+                                            if (d['cours' + i]) return true;
+                                        }
+                                        return false;
+                                    });
+
+                                    if (indexLecon > -1) {
+                                        //Classe du bouton
+                                        btn.removeClass("btn-outline-primary").addClass("btn-primary");
+                                        //Affiche la table
+                                        div2.show();
+                                        dt.columns.adjust();
+                                        //Scroll à la bonne postion
+                                        dt.scroller.toPosition(indexLecon);
+                                    }
+
+                                    //Affiche/cahe la table sur clic du bouton
+                                    btn.click(() => {
+                                        div2.toggle();
+                                        dt.columns.adjust();
                                     });
                                 });
                             });
