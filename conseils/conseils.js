c68f6069a7f951665339a1bf2b3ffbd14b02736e
ncochon <ncochon@hotmail.com>
2021-06-12 18:39:25 +0200

    Création du mail de conseils

---
+++ conseils/conseils.js
@@ -0,0 +1,20 @@
+function onSignIn() {
+    const urlParams = new URLSearchParams(window.location.search);
+    var idClient = urlParams.get('idClient');
+    if (!idClient) {
+        //idClient = 'recGMIxbX0mMzLVGS';
+        idClient = 'rec0BYexFc3OIKXUO';
+    }
+
+    appendPre("Création du mail en cours...");
+
+    var returnValue = { value: null };
+    callScriptFunction('genereMailConseil', [idClient])
+        .then(() => {
+            appendPre("Création du mail terminée.");
+        });
+
+}
+
+function onSignOut() {
+}
